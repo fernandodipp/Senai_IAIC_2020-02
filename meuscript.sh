a343f40b7b3b7a924344ba92d2b84bbce97ef655
fdipp_aws <fernando.dipp@gmail.com>
2020-08-14 00:55:10 +0000

    Primeiro commit

---
+++ meuscript.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+cd
+ls
+mkdir projeto
+cd projeto/
+mkdir src api doc
+ls
+cd src
+mkdir utils modules
+ls
+cd ..
+touch README.txt
+ls
+cd src/
+touch main.py
+ls
+cd /projeto/api
+cd ..
+cd api/
+touch service.py
+pwd
+cd /home/ubuntu/projeto/doc
+touch interface.txt modules.txt projeto.txt
+cd /home/ubuntu/projeto/src/utils/
+touch common.py
+cd ..
+cd modules/
+touch dbconnect.py
